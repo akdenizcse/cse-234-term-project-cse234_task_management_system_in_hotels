f91eed462edf650d50b09b34f14e01c1423c5881
enveruntuc <20200808047@ogr.akdeniz.edu.tr>
2024-05-07 15:55:53 +0300

    Created basic structure for login page

---
+++ app/src/main/java/com/example/taskmanagement/LoginScreen.kt
@@ -0,0 +1,62 @@
+package com.example.taskmanagement
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome", fontSize = 28.sp, fontWeight = FontWeight.Bold )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(text = "Login to your account")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Email address")
+        })
+        
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Password")
+        })
+        
+        Spacer(modifier = Modifier.height(16.dp))
+        
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Login")
+            
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        
+        Text(text = "Forgot Password? ", modifier = Modifier.clickable {  })
+
+
+
+
+    }
+}
