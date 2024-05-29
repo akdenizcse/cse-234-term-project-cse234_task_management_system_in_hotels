211d4f28b4137e8c6f772dd81d539727a7eacf65
enveruntuc <20200808047@ogr.akdeniz.edu.tr>
2024-05-29 21:44:40 +0300

    added internet permission and navigated screens

--- app/src/main/java/com/example/taskmanagement/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagement/MainActivity.kt
@@ -4,52 +4,65 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.taskmanagement.dashboard.DashboardScreen
 import com.example.taskmanagement.login.LoginScreen
-import com.example.taskmanagement.uiScreens.Dashboard
-import com.example.taskmanagement.uiScreens.DashboardInProgress
-
+import com.example.taskmanagement.uiScreens.InprogressTasks
+import com.example.taskmanagement.uiScreens.ProfileScreen
+import com.example.taskmanagement.uiScreens.Schedule
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             val navController = rememberNavController()
-            //ekranları birbirine bağlama
-            NavHost(navController = navController, startDestination = "LoginScreen", builder = {
-                composable(route = "LoginScreen") {
-                    LoginScreen(navController)
+            NavHost(navController = navController, startDestination = "login") {
+                composable("login") {
+                    LoginScreen(navController = navController)
                 }
-                //daha sonra buraya parametre gönderme ekleyeceğiz
-                composable(route = "Dashboard") {
+                composable("DashboardScreen") {
+                    DashboardScreen(navController = navController)
+                }
+                composable("profile") {
+                    ProfileScreen(navController = navController)
+                }
+                composable("schedule") {
+                    Schedule(navController = navController)
+                }
+                composable("inprogressTasks") {
                     Column {
-
-
-                        Dashboard("enver", "Untuç", "manager")
-                        Spacer(modifier = Modifier.height(30.dp))
-                        DashboardInProgress()
+                        InprogressTasks(navController = navController)
+                        Row {
+                            Button(onClick = {
+                                navController.navigate("DashboardScreen")
+                            }) {
+                                Text(text = "Go to Dashboard")
+                            }
+                            Spacer(modifier = Modifier.width(width = 80.dp))
+                            Button(onClick = {
+                                navController.navigate("profile")
+                            }) {
+                                Text(text = "Go to Profile")
+                            }
+                        }
                     }
-                }
-              /*  composable(route = "Schedule"){
-                    Schedule(navController)
+
 
                 }
-                composable(route = "Profile"){
-                    Profile(navController)
 
-                }*/
-            })
-
+            }
         }
-    }
 
+    }
 }
 
 
